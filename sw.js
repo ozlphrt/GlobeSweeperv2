303ed11a29345d53c6de2f6e1b3ce1f8a22d34a5
ozlphrt <ozalph@gmail.com>
2026-08-01 16:21:35 +0300

    feat: clustered mine placement algorithm (72% cluster factor) to eliminate isolated single mines

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for GlobeSweeper PWA
-const CACHE_NAME = 'globesweeper-v1.3.78-lower-mine-ratio';
+const CACHE_NAME = 'globesweeper-v1.3.79-mine-clustering';
 const urlsToCache = [
   './',
   'index.html',
